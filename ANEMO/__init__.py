1d9331afffbb55eb29d40dacb479ba796f5b79b1
chloepasturel <chloe.pasturel@gmail.com>
2018-04-26 15:19:17 +0200

    ANEMO

---
+++ ANEMO/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+ 
+"""
+    Ce module permet de faire un fit sur les données
+"""
+ 
+__version__ = "0.0.1"
+ 
+from .ANEMO import ANEMO
---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+ 
+from setuptools import setup, find_packages
+ 
+import ANEMO.ANEMO
+#import EyeTracking
+
+setup(
+    name='ANEMO',
+    version=ANEMO.__version__,
+    packages=find_packages(),
+    author="",
+    author_email="",
+    description="",
+    long_description=open('README.md').read(),
+    include_package_data=True,
+    url='',
+    classifiers=[
+        "Programming Language :: Python",
+    ],
+)
