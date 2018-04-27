0014007b18e021ff666005cc32c42b625ead50cf
Laurent Perrinet <laurent.perrinet@gmail.com>
2018-04-27 11:25:54 +0200

    small notes

--- ANEMO/__init__.py
+++ ANEMO/__init__.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
- 
+
 """
     Ce module permet de faire un fit sur les données
 """
- 
+
 __version__ = "0.0.1"
- 
+
 from .ANEMO import ANEMO
+#from .EyeTracker import *
+# TODO add edfreader +  EyeTracker
+# TODO charger un edf depuis internet? https://github.com/chloepasturel/AnticipatorySPEM/raw/master/data/enregistrement_AM_2017-10-23_100057.edf
+# TODO binder
