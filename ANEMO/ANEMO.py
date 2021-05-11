43141862530bc4fbf593feef331c70c9f0bc20a9
Vanessa Morita <vvanessamorita@gmail.com>
2021-05-11 09:08:32 +0200

    update latency

--- ANEMO/ANEMO.py
+++ ANEMO/ANEMO.py
@@ -1242,13 +1242,13 @@ class ANEMO(object) :
                 #----------------------------------------------
                 
                 # test latency:
-                max_latency = TargetOn-t_0 + 100
+                max_latency = TargetOn-t_0 + 150
 
                 param_fit=[{'name':'steady_state',      'value':value_steady_state, 'min':3.,                   'max':40.,             'vary':True  },
                            {'name':'dir_target',        'value':dir_target,         'min':None,                 'max':None,            'vary':False },
                            {'name':'a_anti_tmp',        'value':value_anti,         'min':-40.,                 'max':40.,             'vary':vary_anti,},
                            {'name':'a_anti',            'expr':'a_anti_tmp if abs(a_anti_tmp) >= .5 else 0'}, # arbitrary threshold for valid acceleration
-                           {'name':'latency',           'value':value_latency,      'min':TargetOn-t_0+75,      'max':max_latency,     'vary':True  },
+                           {'name':'latency',           'value':value_latency,      'min':TargetOn-t_0+70,      'max':max_latency,     'vary':True  },
                            {'name':'start_anti_tmp',    'value':TargetOn-t_0-100,   'min':TargetOn-t_0-500,     'max':TargetOn-t_0,    'vary':vary_start_anti},
                            {'name':'start_anti',        'expr':'start_anti_tmp if a_anti != 0 else latency-1'}]
 
